33e635a99eec4c247324c012a87a3325f89fa35e
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-06-02 13:26:29 +0200

    refacturing

--- src/App.java
+++ src/App.java
@@ -3,18 +3,15 @@ public class App {
     public static void main(String[] args) {
         Konto konto1 = new Konto();
         ausgabe("Konto1: " + Integer.toString(konto1.kontostand));
-        konto1.vervielfältigung(2);
-        ausgabe("Konto1, neu: " + Integer.toString(konto1.kontostand));
+        ausgabe("Konto1, neu: " + Integer.toString(konto1.vervielfältigung(2)));
 
         Konto konto2 = new Konto();
         ausgabe("Konto2: " + Integer.toString(konto2.kontostand));
-        konto2.vervielfältigung(3);
-        ausgabe("Konto2, neu: " + Integer.toString(konto2.kontostand));
+        ausgabe("Konto2, neu: " + Integer.toString(konto2.vervielfältigung(2)));
 
         Konto konto3 = new Konto();
         ausgabe("Konto3: " + Integer.toString(konto3.kontostand));
-        konto3.vervielfältigung(10);
-        ausgabe("Konto3, neu: " + Integer.toString(konto3.kontostand));
+        ausgabe("Konto3, neu: " + Integer.toString(konto3.vervielfältigung(2)));
     }
 
     public static void ausgabe(String getStr){
